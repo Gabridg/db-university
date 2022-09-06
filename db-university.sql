cd3019e51ec2ad874fdc8c3f40e89862c72ebe27
Gabridg <gabri99.del@gmail.com>
2022-09-06 17:13:35 +0200

    1 w/ GROUP BY complete

--- db-university.sql
+++ db-university.sql
@@ -29,7 +29,7 @@ SELECT COUNT(*) FROM `teachers` WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS `iscritti_annui`, `enrolment_date` FROM `students` GROUP BY `enrolment_date`; 
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
