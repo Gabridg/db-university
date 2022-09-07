bf0f63cc6cbb139f61974d9b8ec5f671d3c1dfc7
Gabridg <gabri99.del@gmail.com>
2022-09-07 17:01:30 +0200

    3 task complete

--- QueryJOIN.sql
+++ QueryJOIN.sql
@@ -5,7 +5,7 @@ SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` FROM `degrees` 
 SELECT `degrees`.`name`, `departments`.`name` FROM `departments` JOIN `degrees` ON `departments`.`id`= `degrees`.`department_id` WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'; 
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-
+SELECT `teachers`.`name`, `teachers`.`surname`, `courses`.`name` FROM `teachers` JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`course_id` JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id` WHERE `teachers`.`name` = 'Fulvio' AND `teachers`.`surname`= 'Amato'; 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
